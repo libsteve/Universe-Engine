63541412c22cdeec433047ff508bd6c552a1f785
Steven Brunwasser <sbrunwasser@gmail.com>
2011-11-03 17:48:31 -0400

    Initial Commit

---
+++ Universe EngineTests/Universe_EngineTests.h
@@ -0,0 +1,13 @@
+//
+//  Universe_EngineTests.h
+//  Universe EngineTests
+//
+//  Created by steven on 11/3/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Universe_EngineTests : SenTestCase
+
+@end
---
+++ Universe EngineTests/Universe_EngineTests.m
@@ -0,0 +1,32 @@
+//
+//  Universe_EngineTests.m
+//  Universe EngineTests
+//
+//  Created by steven on 11/3/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Universe_EngineTests.h"
+
+@implementation Universe_EngineTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Universe EngineTests");
+}
+
+@end
